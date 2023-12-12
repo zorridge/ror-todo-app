a9e80838daf097f4b0fdc5bade6858aaa988192a
Zhou Ziheng <zhou.ziheng@outlook.com>
2023-12-12 13:56:15 +0800

    implement remaining CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,45 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :deadline)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :deadline, presence: true
+
+  validate :deadline_cannot_be_in_the_past
+
   attribute :isDone, :boolean, default: false
+
+  private
+
+  def deadline_cannot_be_in_the_past
+    errors.add(:deadline, "can't be in the past") if deadline.present? && deadline < Date.today
+  end
 end
